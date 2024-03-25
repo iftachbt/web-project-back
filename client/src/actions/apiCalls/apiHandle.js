cfdd89b62f7c5ab33515d5df67335b38103e6f2e
iftachbt <iftachbt@gmail.com>
2024-03-26 00:24:31 +0200

    adding publicip

--- client/src/actions/apiCalls/apiHandle.js
+++ client/src/actions/apiCalls/apiHandle.js
@@ -1,13 +1,19 @@
 import axios from "axios";
-const URL = process.env.REACT_APP_SERVER;
 
 const errorHandler = (err) => {
   console.log(err);
   return "err";
 };
-
+async function getUrl() {
+  if (process.env.REACT_APP_PROD === "PROD") {
+    const publicIPResponse = await axios.get("https://api.ipify.org?format=json");
+    return "http://" + publicIPResponse.data.ip + ":5000/";
+  } else return process.env.REACT_APP_SERVER;
+}
 export async function sendPost(route, body) {
   try {
+    let URL = await getUrl();
+    console.log(URL);
     const res = await axios.post(URL + route, body, {
       headers: { "Content-Type": "application/json" },
       withCredentials: true,
@@ -18,6 +24,8 @@ export async function sendPost(route, body) {
   }
 }
 export async function sendDelete(route) {
+  let URL = await getUrl();
+  console.log(URL);
   try {
     const res = await axios.delete(URL + route, {
       headers: { "Content-Type": "application/json" },
@@ -30,6 +38,8 @@ export async function sendDelete(route) {
   }
 }
 export async function sendUpdate(route, body) {
+  let URL = await getUrl();
+  console.log(URL);
   try {
     const res = await axios.put(URL + route, body, {
       headers: { "Content-Type": "application/json" },
@@ -43,6 +53,8 @@ export async function sendUpdate(route, body) {
 }
 
 export async function sendGet(route) {
+  let URL = await getUrl();
+  console.log(URL);
   try {
     const res = await axios.get(URL + route, {
       headers: { "Content-Type": "application/json" },
